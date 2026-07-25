522d8429bf49cf3ce4cb43aa8fc30ee2956fcc36
Suraj Gupta <2k24.cs1p2413999@gmail.com>
2026-07-26 01:37:41 +0530

    Implement lowestCommonAncestor method for BST Using BST Property

---
+++ Trees/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,10 @@
+class 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root!=null){
+            if(root.val>p.val && root.val>q.val) root=root.left;
+            else if(root.val<p.val && root.val<q.val) root=root.right;
+            else return root;
+        }
+        return null;
+    }
+}
