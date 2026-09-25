f22a74b0bf763359be67295f62d92d739875a8da
Suraj Gupta <2k24.cs1p2413999@gmail.com>
2026-09-25 23:56:14 +0530

    Time: 0 ms (100%), Space: 46.8 MB (75.1%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return dfs(root1,root2);
+    }
+    private TreeNode dfs(TreeNode node1,TreeNode node2){
+        if(node1==null) return node2;
+        if(node2==null) return node1;
+        node1.val=node1.val+node2.val;
+        node1.left=dfs(node1.left,node2.left);
+        node1.right=dfs(node1.right,node2.right);
+        return node1;
+    }
+}
